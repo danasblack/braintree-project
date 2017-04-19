78ba598fd3069e621843697598f638fddf01c4d0
danasblack <danasblack@gmail.com>
2017-04-19 06:29:02 -0500

    received payment nonce information

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,21 +1,21 @@
 class CheckoutsController < ApplicationController
 	require 'dotenv'
 
+	def index
+	end 
+
 	def new
 		@client_token = Braintree::ClientToken.generate
 	end
 
 	def create
-    amount = params["amount"] # In production you should not take amounts directly from clients
+    amount = params["amount"] 
     nonce_from_the_client = params[:payment_method_nonce]
 
-    @result = Braintree::Transaction.sale(
-    	:amount => "20.00",
-    	:payment_method_nonce => nonce_from_the_client,
-    	:options => {
-    		:submit_for_settlement => true
-    	}
-    	)
+    @result =  Braintree::PaymentMethod.create(
+         :customer_id => "131866",
+         :payment_method_nonce => nonce_from_the_client
+  )
   end
 end
 
